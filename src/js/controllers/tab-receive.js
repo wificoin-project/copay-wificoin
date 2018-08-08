a1996eadebbf6410f8df64c7b8f1c203bfeaa6a7
liudf0716 <liudf0716@gmail.com>
2018-08-08 18:09:48 +0800

    change to WFC finish donate feature Signed-off-by liudf0716@gmail.com

--- src/js/controllers/tab-receive.js
+++ src/js/controllers/tab-receive.js
@@ -35,8 +35,10 @@ angular.module('copayApp.controllers').controller('tabReceiveController', functi
   
   $scope.requestDonate = function() {	
     if (!$scope.wallet || $scope.generatingAddress || !$scope.wallet.isComplete()) return;
-		var donateUrl = 'https://wfc.xyblock.net/#/wifiPortal/donate?toAmount=10&toAddress=' + $scope.protoAddr;		
-		popupService.showAlert(donateUrl);
+	    var amount = Math.round(Math.random()*100+1);
+		var donateUrl = 'https://wfc.xyblock.net/#/wifiPortal/donate?toAmount='+amount+'&toAddress=' + $scope.addr;		
+		donateUrl += '&payTitle=跪求土豪打赏';
+		$scope.donate = donateUrl;
   };
 
   $scope.goCopayers = function() {
@@ -143,7 +145,9 @@ angular.module('copayApp.controllers').controller('tabReceiveController', functi
   };
 
   $scope.shareAddress = function() {
-    if (!$scope.isCordova) return;
-    window.plugins.socialsharing.share($scope.protoAddr, null, null, null);
+    if (!$scope.isCordova) {
+	  return;
+	}
+    window.plugins.socialsharing.share($scope.addr, null, null, null);
   }
 });
